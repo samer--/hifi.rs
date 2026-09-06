58133a36c02cba02bb810744ef1bd85e173d812a
Samer Abdallah <samer.abdallah.00@gmail.com>
2026-09-06 18:28:41 +0100

    Switch to SVG media control icons to make web view narrower

---
+++ www/src/lib/components/IconButton.svelte
@@ -0,0 +1,13 @@
+<script>
+	export let onClick = null;
+	export let label = '';
+</script>
+
+<button
+	aria-label={label}
+	title={label}
+	class="bg-amber-900 hover:bg-blue-800 text-gray-100 leading-none p-2 lg:p-3 border-r-4 border-b-4 border-blue-800"
+	on:click|stopPropagation={onClick}
+>
+	<slot />
+</button>
