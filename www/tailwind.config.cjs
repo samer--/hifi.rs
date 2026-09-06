e139e007d52e29dbeaea0d7d937604eef5e0560b
Samer Abdallah <samer.abdallah.00@gmail.com>
2026-09-06 18:00:21 +0100

    Autoformat js with prettier

--- www/tailwind.config.cjs
+++ www/tailwind.config.cjs
@@ -1,22 +1,19 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 /** @type {import('tailwindcss').Config}*/
 const config = {
-  content: [
-    './src/**/*.{html,js,svelte,ts}',
-  ],
+	content: ['./src/**/*.{html,js,svelte,ts}'],
 
-  theme: {
-    extend: {
-      fontFamily: {
-        serif: ['Bitter', ...defaultTheme.fontFamily.serif],
-        sans: ['HandJet', ...defaultTheme.fontFamily.sans]
-      }
-    }
-  },
+	theme: {
+		extend: {
+			fontFamily: {
+				serif: ['Bitter', ...defaultTheme.fontFamily.serif],
+				sans: ['HandJet', ...defaultTheme.fontFamily.sans]
+			}
+		}
+	},
 
-  plugins: [
-  ]
+	plugins: []
 };
 
 module.exports = config;
